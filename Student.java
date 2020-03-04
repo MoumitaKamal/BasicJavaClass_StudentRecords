0fd9e1b1a7fcd0438f9cde4bdf437a92b7521948
MoumitaKamal <mkamal42@students.tntech.edu>
2020-03-04 00:53:15 -0600

    student class

--- Student.java
+++ Student.java
@@ -2,6 +2,7 @@
  *Title: Student Class 
 
  *Description: Contains student information (name, ID, email ID, student account password, and sections the student is enrolled in). 
+ *Date: August 2015
  
  *Author: Moumita Kamal
  ***********************************************************/
