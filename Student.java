022f34485db515460ce98da298667fb6ac1cda7f
MoumitaKamal <mkamal42@students.tntech.edu>
2020-03-04 00:19:23 -0600

    student class

---
+++ Student.java
@@ -0,0 +1,15 @@
+/***********************************************************
+ *Title: Student Class 
+
+ *Description: Contains student information (name, ID, email ID, student account password, and sections the student is enrolled in). 
+ 
+ *Author: Moumita Kamal
+ ***********************************************************/
+
+public class Student{
+    public String name;        
+    public int sid;            // student ID
+    public String email;       
+    public String password;    
+    public int section;        // stores section number 
+}
